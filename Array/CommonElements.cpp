eb9b4bfa8b4bbaff18114779b67ed2284a4f766d
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-02-09 08:44:55 +0530

    optimised solution for common in 3 sorted array

--- Array/CommonElements.cpp
+++ Array/CommonElements.cpp
@@ -1,48 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
+ 
+int main(){
+    int arr1[3] = {1,2,3};
+    int arr2[3] = {4,5,6};
+    int arr3[3] = {7,8,9};
 
-int main()
-{
-    int arr1[6] = {1, 5, 10, 20, 40, 80};
-    int arr2[5] = {6, 7, 20, 80, 100};
-    int arr3[8] = {3, 4, 15, 20, 30, 70, 80, 120};
-
-    int n1 = 6, n2 = 5, n3 = 8;
+    int n1 = 3, n2 = 3, n3 = 3;
     int i1 = 0;
     int i2 = 0;
     int i3 = 0;
-
     vector<int> cnt;
-    while(i1 < n1 && i2 < n2 && i3 < n3){
-        if(arr1[i1] == arr2[i2] == arr3[i3]){
-            cout << "Pushed" <<endl;
-            cnt.push_back(arr1[i1]);
-            i1++,i2++,i3++;
+    while (i1 < n1 && i2 < n2 && i3 < n3)
+    {
+        if(arr1[i1] < arr2[i2]){
+            i1++;
+        }
+        else if(arr1[i1] > arr2[i2]){
+            i2++;
         }
         else{
-            cout << i1 <<endl;
-            cout << i2 <<endl;
-            cout << i3 <<endl;
-            int mx = max(arr1[i1] , max(arr2[i2] , arr3[i3]));
-            if(arr1[i1] < mx){
+            if (arr1[i1] == arr1[i1-1])
+            {
                 i1++;
+                continue;
             }
-            if(arr2[i2] < mx){
-                i2++;
-            }
-            if(arr3[i3] < mx){
-                i3++;
-            }
-            else{
+            
+            while (i3 < n3 && arr3[i3] < arr1[i1]) i3++;
+            
+            if(arr3[i3] == arr1[i1]) {
                 cnt.push_back(arr1[i1]);
-                i1++,i2++,i3++;
             }
+            i2++;
+            i1++;
+            
         }
     }
+
 
     for (int i = 0; i < cnt.size(); i++)
     {
-        cout << cnt[i] << " ";
+        cout << cnt[i];
     }
+    
     
 }
--- Array/tempCodeRunnerFile.cpp
+++ Array/tempCodeRunnerFile.cpp
@@ -1,3 +1,4 @@
-void digit(vector<int> temp , int carr){
-//     temp.push_back(carr);
-// }
+
+    //     cout << arr1[i1] << endl;
+    //     cout << arr2[i2] << endl;
+    //     cout << arr3[i3] << endl;
