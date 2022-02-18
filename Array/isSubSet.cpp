540e3e69b7e0c92b782d41bf49441d4a9d5f784a
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-02-18 23:47:07 +0530

    Subarray present

--- Array/isSubSet.cpp
+++ Array/isSubSet.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     vector<int> arr;
     int n;
     vector<int> sub;
@@ -18,36 +19,43 @@ int main(){
 
     cin >> x;
 
-    for (int  i = 0; i < x; i++)
+    for (int i = 0; i < x; i++)
     {
         int temp;
         cin >> temp;
         sub.push_back(temp);
     }
 
-    bool chk = false;
-    
-    for (int i = 0; i < x; i++)
-    {
-        chk = false;
-        for (int j = 0; j < n; j++)
-        {
-            if(sub[i] == arr[j]){
-                chk = true;
-            }
-        }
+    bool chk = true;
+    int cnt = 0;
+    sort(arr.begin(), arr.end());
+    sort(sub.begin(), sub.end());
 
-        if(!chk){
-            cout << "Not a subset";
+    int k = 0;
+    int j = 0;
+    while (k != arr.size())
+    {
+        if (k == arr.size() - 1 && cnt != sub.size())
+        {
+            chk = false;
+            cout << "No" << endl;
             break;
         }
 
-        
+        else if (arr[k] < sub[j])
+        {
+            k++;
+        }
+
+        else
+        {
+            j++;
+            cnt++;
+        }
     }
 
-    if(chk){
-        cout << "Is a subset";
+    if (chk)
+    {
+        cout << "Yes" <<endl ;
     }
-    
-    
 }
