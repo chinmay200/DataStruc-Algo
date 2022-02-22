8bcf6786899c103d514e9e4f2f478435bffbc6c6
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-02-23 00:14:36 +0530

    Rainwater problem Linear TC using prefix and suffix sum to find max left AND max right

--- Array/TrappingRainWater.cpp
+++ Array/TrappingRainWater.cpp
@@ -32,10 +32,42 @@ int main(){
 
     int water = 0;
 
+    int pref[n];
+
+    pref[0] = arr[0];
+    int suff[n];
+
+    suff[n-1] = arr[n-1];
+
+    for (int i = 1; i < n; i++)
+    {
+        pref[i] = max(pref[i-1] , arr[i]);
+    }
+    cout << endl;
+    
+    for (int i = n-2; i >= 0; i--)
+    {
+        suff[i] = max(suff[i+1] , arr[i]);
+    }
+
+    
+
     for (int i = 0; i < n; i++)
     {
-        int lft = leftMx(i , arr);
-        int rgt = rightMx(i , arr , n);
+        cout << pref[i] << " ";
+    }
+
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << suff[i] << " ";
+    }
+    
+
+    for (int i = 0; i < n; i++)
+    {
+        int lft = pref[i];
+        int rgt = suff[i];
         int mi = min(lft , rgt);
 
         int ans = abs(mi - arr[i]);
