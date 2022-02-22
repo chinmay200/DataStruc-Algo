878a3f750a7ddce9e4b00780b91ccdeeb048877c
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-02-22 23:44:08 +0530

    brute force for RainWater Trapping O(N^2)

--- Array/TrappingRainWater.cpp
+++ Array/TrappingRainWater.cpp
@@ -1,6 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int leftMx(int pos , int arr[]){
+    int mx = arr[pos];
+
+    for (int i = pos; i >= 0; i--)
+    {
+        mx = max(arr[i] , mx);
+    }
+    return mx;
+}
+int rightMx(int pos , int arr[] , int n){
+    int mx = arr[pos];
+
+    for (int i = pos; i < n; i++)
+    {
+        mx = max(arr[i] , mx);
+    }
+    return mx;
+}
+
 int main(){
     int n;
     cin >> n;
@@ -11,6 +30,19 @@ int main(){
         cin >> arr[i];
     }
 
+    int water = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int lft = leftMx(i , arr);
+        int rgt = rightMx(i , arr , n);
+        int mi = min(lft , rgt);
+
+        int ans = abs(mi - arr[i]);
+
+        water+=ans;
+    }
     
+    cout << water ;
     
 }
