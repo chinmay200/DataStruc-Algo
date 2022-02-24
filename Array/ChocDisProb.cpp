1f00af1294ec771dcb61da67011010401968e3da
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-02-24 23:44:00 +0530

    Chocdis problem done

--- Array/ChocDisProb.cpp
+++ Array/ChocDisProb.cpp
@@ -1,6 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    int n;
+    cin >> n;
+    int m;
+
+    cin >> m;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+    int i = 0;
+    int ans = INT_MAX;
+    while (i < n-m)
+    {
+        ans = min(ans , arr[m-1+i] - arr[i]);
+        i++;
+    }
+
+    cout << ans;
 }
