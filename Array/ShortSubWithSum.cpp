5264b8b5495c2f22cfd5c7bda85dea415f42fa2d
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-02-26 23:19:45 +0530

    Shortest subarray len whose sum greater than x

--- Array/ShortSubWithSum.cpp
+++ Array/ShortSubWithSum.cpp
@@ -8,9 +8,35 @@ int main(){
 
     for (int i = 0; i < n; i++)
     {
-        cin >> n;
+        cin >> arr[i];
     }
 
-    
+    int x;
+    cin >> x;
+
+    int strt,end;
+    int mi = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        int sum = arr[i];
+        if(sum > x){
+                end = i;
+                strt = i-1;
+                mi = min(mi , end-strt);
+            }
+        for (int j = i+1; j < n; j++)
+        {
+            sum+=arr[j];
+            if(sum > x){
+                end = j;
+                strt = i-1;
+                mi = min(mi , end-strt);
+            }
+        }
+        
+    }
+
+    cout << mi;
+        
     
 }
