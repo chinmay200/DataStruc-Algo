d647620645572c69fec91eba7a3101f68f44fcfd
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-02-28 23:10:48 +0530

    sorting solution for array partition

--- Array/threeWayPrt.cpp
+++ Array/threeWayPrt.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main(){
-    int arr[];
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int a,b;
+    cin >> a >> b;
+
+    sort(arr , arr+n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
 }
