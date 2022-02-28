5dbaa9b76fd1bb3d7e46f522717cce7575fd150c
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-02-28 23:41:13 +0530

    O(n) solution for partitioning 2

--- Array/threeWayPrt.cpp
+++ Array/threeWayPrt.cpp
@@ -16,33 +16,56 @@ int main(){
 
     int ans[n] ;
     int j = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if(arr[i] <= a){
-            ans[j] = arr[i];
-            j++;
-        }
-    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if(arr[i] <= a){
+    //         ans[j] = arr[i];
+    //         j++;
+    //     }
+    // }
 
-    for (int i = 0; i < n; i++)
-    {
-        if(arr[i] > a && arr[i] <= b){
-            ans[j] = arr[i];
-            j++;
-        }
-    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if(arr[i] > a && arr[i] <= b){
+    //         ans[j] = arr[i];
+    //         j++;
+    //     }
+    // }
 
-    for (int i = 0; i < n; i++)
-    {
-        if(arr[i] >= b){
-            ans[j] = arr[i];
-            j++;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if(arr[i] >= b){
+    //         ans[j] = arr[i];
+    //         j++;
+    //     }
+    // }
+
+
+    int l = 0;
+    int r = n-1;
+    int i = 0;
+    while(i < n && i < r){
+        if(arr[i] > b){
+            int temp = arr[i];
+            arr[i] = arr[r];
+            arr[r] = temp;
+            r--;
+            i-=2;
         }
+
+        else if (arr[i] < a)
+        {
+            int temp = arr[i];
+            arr[i] = arr[l];
+            arr[l] = temp;
+            l++;
+        }
+        i++;
     }
     
     for (int i = 0; i < n; i++)
     {
-        cout << ans[i] << " ";
+        cout << arr[i] << " ";
     }
     
     
