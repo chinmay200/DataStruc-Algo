3c40142474f78b2420135edc0bd0ff66cf748aa4
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-03-02 15:04:41 +0530

    Palindrome funtion created

--- Array/PalinArray.cpp
+++ Array/PalinArray.cpp
@@ -1,22 +1,7 @@
 #include <bits/stdc++.h>
+#include "Palindrome.cpp"
 using namespace std;
 
-bool palin(int num){
-    int temp = num;
-    int rev = 0;
-    while (temp>0)
-    {
-        int digit = temp%10;
-        rev = (10*rev + digit);
-        temp/=10;
-    }
-
-    if(rev == num){
-        return true;
-    }
-    return false;
-    
-}
 
 int main(){
     int n;
