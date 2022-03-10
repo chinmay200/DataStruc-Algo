94f5fc0e1b099f6e617944687cea7014211d17b1
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-03-10 20:52:33 +0530

    binary to decimal

---
+++ PracticeDifferentQuestions/BinToDec.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void binToDec(vector<int> &b, int &num){
+    int i = 0;
+    int n = b.size()-1;
+    while (n >= 0)
+    {
+        int d = b[n];
+        num += (pow(2 , i)*d);
+        i++;
+        n--;
+    }
+    
+}
+
+int main(){
+    string bin;
+
+    cin >> bin;
+
+    vector<int> b;
+    for (int i = 0; i < bin.length(); i++)
+    {
+        if(bin[i] == '1'){
+            b.push_back(1);
+        }
+        else{
+            b.push_back(0);
+        }
+    }
+    
+    for (int i = 0; i < bin.length(); i++)
+    {
+        cout << bin[i];
+    }
+    
+    int num = 0;
+    binToDec(b , num);
+    cout << endl;
+    cout << num;
+}
