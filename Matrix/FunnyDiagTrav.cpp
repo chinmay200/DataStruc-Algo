59ff2f986149044904c15602e072e780ab8f89f9
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-03-11 23:00:54 +0530

    Completed Funny Diagonal trav

--- Matrix/FunnyDiagTrav.cpp
+++ Matrix/FunnyDiagTrav.cpp
@@ -1,6 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n][n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    int row = 0;
+    int col = 0;
+    int i = 1;
+    while (col < n)
+    {
+        while (col > -1)
+        {
+            cout << arr[row][col] << " ";
+            col--;
+            row++;
+        }
+        col = i;
+        i++;
+        row = 0;
+    }
+
+    i = 2;
+    row = 1;
+    col = n-1;
+    while(row < n){
+        while(row < n){
+            cout << arr[row][col] << " ";
+            row++;
+            col--;
+        }
+        row = i;
+        i++;
+        col = n-1;
+    }
 }
