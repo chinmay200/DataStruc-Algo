a5a5cbd9deb53d88fd9d4c1a9781e5f1299ab0b6
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-03-15 23:26:51 +0530

    diagonal mat right to left

--- Matrix/DiadTravRighttoLeft.cpp
+++ Matrix/DiadTravRighttoLeft.cpp
@@ -14,5 +14,34 @@ int main(){
             cin >> arr[i][j];
         }
     }
+    int row = 0;
+    int col = n-1;
+    int i = col-1;
+    while (col > -1)
+    {
+        while (col != n-1)
+        {
+            cout << arr[row][col] << " ";
+            col++;
+            row++;
+        }
+        cout << arr[row][col] << " ";
+        col = i;
+        i--;
+        row = 0;
+    }
 
+    i = 2;
+    row = 1;
+    col = 0;
+    while(i <= n){
+        while(row < n){
+            cout << arr[row][col] << " ";
+            row++;
+            col++;
+        }
+        row = i;
+        i++;
+        col = 0;
+    }
 }
