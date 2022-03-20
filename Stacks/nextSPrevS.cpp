9890288081d3c2256e4395dc5bad32102fefeffb
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-03-20 20:45:19 +0530

    Prev smaller ans Next samller in an array

--- Stacks/nextSPrevS.cpp
+++ Stacks/nextSPrevS.cpp
@@ -1,16 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;
-    cin >> n;
-    int arr[n];
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    
+void prevSmaller(int arr[] , int n){
     stack<int> num;
     for (int i = 0; i < n; i++)
     {
@@ -28,5 +19,52 @@ int main(){
         num.push(i);
         
     }
+}
+
+
+void nextSmaller(int arr[] , int n){
+    stack<int> num;
+    vector<int> ans;
+    for (int i = n-1; i >= 0; i--)
+    {
+        while (!num.empty() && arr[num.top()] >= arr[i])
+        {
+            num.pop();
+        }
+
+        if(num.empty()){
+            // cout << -1 << " ";
+            ans.push_back(-1);
+        }
+        else{
+            // cout << num.top() << " ";
+            ans.push_back(num.top());
+        }
+        num.push(i);
+        
+    }
+
+    reverse(ans.begin() , ans.end());
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
     
+    
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    prevSmaller(arr,n);
+    cout << endl;
+    nextSmaller(arr,n);
 }
