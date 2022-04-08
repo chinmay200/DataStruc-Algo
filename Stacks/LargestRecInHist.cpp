8f047d52a8126642af2d79d0de8315b0d9c3dd4c
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-04-09 00:03:49 +0530

    better solution for largest area in a histogram

--- Stacks/LargestRecInHist.cpp
+++ Stacks/LargestRecInHist.cpp
@@ -36,17 +36,48 @@ int main()
         cin >> arr[i];
     }
 
+    stack <mi>;
+    int prev[n];
+    for (int i = 0; i < n; i++)
+    {
+        while (!(mi.empty()) && arr[mi.top()] >= arr[i])
+        {
+            mi.pop();
+        }
+
+        if(mi.empty())
+            prev[i] = 0;
+        else
+            prev[i] = mi.top()+1;
+        mi.push(i);
+        
+    }
+
+    while(!(mi.empty())){
+        mi.pop();
+    }
+
+    int next[n];
+    for (int i = n-1; i >= 0; i--)
+    {
+        while (!(mi.empty()) && arr[mi.top()] >= arr[i])
+        {
+            mi.pop();
+        }
+
+        if(mi.isEmpty())
+            next[i] = n-1;
+        else
+            next[i] = mi.top()-1;
+        mi.push(i);
+    }
 
     for (int i = 0; i < n; i++)
     {
-        int l = prevmin(arr , i);
-        int r = nextmin(arr , i , n);
-
-        l++;
-        r--;
-
-        ans = max(ans , (r-l+1)*arr[i]);
+        ans = max(ans , (next[i] - prev[i] + 1) * arr[i]);
     }
-        cout << ans;
+    
+    cout << ans;
+    
 }
 // [2,1,5,6,2,3]
