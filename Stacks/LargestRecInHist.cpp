30ca3d7adeca53b9763c7024823548f96a1df981
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-04-08 22:52:36 +0530

    Looking for prevmin

--- Stacks/LargestRecInHist.cpp
+++ Stacks/LargestRecInHist.cpp
@@ -1,62 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int prevmin(int arr[] , int i){
+    int x = -1;
+    int pos = i;
+    while(arr[i] <= arr[pos]){
+        pos--;
+        x = pos;
+    }
+
+    return x;
+}
+
 int main()
 {
     int n;
     cin >> n;
     int arr[n];
+    int ans = 0;
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
-    stack<int> s;
-    int prev[n];
     for (int i = 0; i < n; i++)
     {
-        while (!s.empty() && arr[s.top()] >= arr[i])
-        {
-            s.pop();
-        }
+        int l = prevmin(arr , i);
+        int r = nextmin(arr , i , n);
 
-        if (s.empty())
-        {
-            prev[i] = -1;
-        }
-
-        else
-        {
-            prev[i] = s.top();
-        }
-
-        s.push(i);
+        l++ , r++;
+        ans = max(ans , (r-l)*arr[i]);
     }
-    stack<int> mn;
-    int next[n];
-    for (int i = n - 1; i >= 0; i--)
-    {
-        while (!mn.empty() && arr[mn.top()] >= arr[i])
-        {
-            mn.pop();
-        }
-
-        if (mn.empty())
-        {
-            next[i] = n;
-        }
-        else
-        {
-            next[i] = mn.top();
-        }
-        mn.push(i);
-    }
-
-    int ans = 0;
-    for (int i = 0; i < n; i++)
-    {
-        ans = max(ans , (next[i] - prev[i] - 1) * arr[i]);
-    }
+    
     
     cout << ans;
 }
